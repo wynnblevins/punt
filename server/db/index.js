6a94781d9ad459c66fa6d1882ded471e57adcaff
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-05 14:15:19 -0400

    Whitespace in index.js

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 let MONGO_URL
-const MONGO_LOCAL_URL = 'mongodb://localhost/mern-passport'
+const DB_NAME = 'punt';
+const MONGO_LOCAL_URL = 'mongodb://localhost/' + DB_NAME;
 
 if (process.env.MONGODB_URI) {
 	mongoose.connect(process.env.MONGODB_URI)
@@ -18,11 +19,11 @@ if (process.env.MONGODB_URI) {
 const db = mongoose.connection
 db.on('error', err => {
 	console.log(`There was an error connecting to the database: ${err}`)
-})
+});
 db.once('open', () => {
 	console.log(
 		`You have successfully connected to your mongo database: ${MONGO_URL}`
 	)
-})
+});
 
-module.exports = db
+module.exports = db;
