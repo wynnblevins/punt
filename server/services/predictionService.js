4334c13cb84d98382c52e4a3557a9762dc5a6cc1
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-13 18:48:44 -0400

    Added decision tree node module w/example code from documentation to project.

--- server/services/predictionService.js
+++ server/services/predictionService.js
@@ -1,6 +1,37 @@
+var DecisionTree = require('decision-tree');
+
 const predictionService = {
   predict: function () {
-    console.log('Inside prediction service predict method');
+    var training_data = [
+      {"color":"blue", "shape":"square", "liked":false},
+      {"color":"red", "shape":"square", "liked":false},
+      {"color":"blue", "shape":"circle", "liked":true},
+      {"color":"red", "shape":"circle", "liked":true},
+      {"color":"blue", "shape":"hexagon", "liked":false},
+      {"color":"red", "shape":"hexagon", "liked":false},
+      {"color":"yellow", "shape":"hexagon", "liked":true},
+      {"color":"yellow", "shape":"circle", "liked":true}
+    ];
+    
+    var test_data = [
+      {"color":"blue", "shape":"hexagon", "liked":false},
+      {"color":"red", "shape":"hexagon", "liked":false},
+      {"color":"yellow", "shape":"hexagon", "liked":true},
+      {"color":"yellow", "shape":"circle", "liked":true}
+    ];
+    
+    var class_name = "liked";
+    
+    var features = ["color", "shape"];
+    
+    var dt = new DecisionTree(training_data, class_name, features);
+    
+    var predicted_class = dt.predict({
+      color: "blue",
+      shape: "hexagon"
+    });
+
+    console.log(predicted_class);
   }
 };
 
