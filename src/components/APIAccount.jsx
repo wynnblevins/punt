b0aeaa4c28957938d6d7412fa37d996d5ad0b3a2
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-23 10:04:51 -0400

    Working on setting up deployments to heroku

--- src/components/APIAccount.jsx
+++ src/components/APIAccount.jsx
@@ -4,22 +4,22 @@ class APIAccount extends Component {
   render() {
     return (
       <form className="form-horizontal">
-				<div className="form-group">
-          <label for="usernameField" className="control-label col-xs-4">Username</label> 
-          <div className="col-xs-8">
+				<div className="form-group col-xs-offset-2 col-xs-8">
+          <label for="usernameField" className="control-label">Username</label> 
+          <div className="col-xs-offset-2 col-xs-8">
             <input id="usernameField" name="usernameField" 
               type="text" className="form-control"/>
           </div>
         </div>
         <div className="form-group">
-          <label for="passwordField" className="control-label col-xs-4">Password</label> 
-          <div className="col-xs-8">
+          <label for="passwordField" className="control-label">Password</label> 
+          <div>
             <input id="passwordField" name="passwordField" 
               type="text" className="form-control"/>
           </div>
         </div> 
         <div className="form-group row">
-    			<div className="col-xs-offset-4 col-xs-8">
+    			<div className="col-xs-offset-2 col-xs-8">
 							<button name="submit" type="button" 
 								className="btn btn-primary">Submit</button>
     			</div>
