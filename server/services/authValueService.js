4c408b2d96a17c3dfb570930c0a449dde62ca91a
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-27 17:01:45 -0400

    Auth value changes

--- server/services/authValueService.js
+++ server/services/authValueService.js
@@ -1,6 +1,6 @@
 const authValueService = {
   getAuthValue: function () {
-    let creds = Buffer.from(`wynnblevins:Nowuckinfurries3612`).toString('base64');
+    let creds = Buffer.from(`wynnblevins:${process.env.PASS}`).toString('base64');
     let authValue = `Basic ${creds}`;
     return authValue;
   }
