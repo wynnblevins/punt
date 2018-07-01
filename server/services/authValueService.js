bf2112e7a0859772d4f74c0c6928a7bc0f5d064f
Wynn Blevins <wynnblevins@yahoo.com>
2018-07-01 16:50:55 -0400

    STILL trying to deploy to heroku

--- server/services/authValueService.js
+++ server/services/authValueService.js
@@ -1,9 +1,7 @@
 const authValueService = {
   getAuthValue: function () {
-    console.log(`cred string: ${process.env.PASS}`)
-    let creds = Buffer.from(`wynnblevins:${process.env.PASS}`).toString('base64');
+    let creds = Buffer.from(`wynnblevins:Nowuckinfurries3612`).toString('base64');
     let authValue = `Basic ${creds}`;
-    console.log('AUTH VALUE: ' + authValue);
     return authValue;
   }
 }
