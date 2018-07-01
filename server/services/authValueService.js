3fd88129c1debd9038412f62ec311b1175da2164
Wynn Blevins <wynnblevins@yahoo.com>
2018-07-01 16:46:20 -0400

    More log statements for heroku deployment

--- server/services/authValueService.js
+++ server/services/authValueService.js
@@ -1,5 +1,6 @@
 const authValueService = {
   getAuthValue: function () {
+    console.log(`cred string: ${process.env.PASS}`)
     let creds = Buffer.from(`wynnblevins:${process.env.PASS}`).toString('base64');
     let authValue = `Basic ${creds}`;
     console.log('AUTH VALUE: ' + authValue);
