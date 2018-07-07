dd33d78bd51ee925c2dde8d0803893dd469256ca
Wynn Blevins <wynnblevins@yahoo.com>
2018-07-07 10:32:31 -0400

    Added safety check in if statement before reading fumbles and adding to total

--- server/services/fumblesService.js
+++ server/services/fumblesService.js
@@ -3,7 +3,7 @@ const fumblesService = {
     let totalReturnYds = 0;
     
     for (var i = 0; i < players.length; i++) {
-      if (players[i].stats) {
+      if (players[i].stats && players[i].stats.FumTD) {
         totalReturnYds += parseInt(players[i].stats.FumTD['#text']);
       }
     }
